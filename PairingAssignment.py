1df3edb43cb700a75ea991fda6bb3af9c36f89f3
Augustin <augustinrives@hotmail.fr>
2024-10-23 17:13:34 +0200

    Premières classes

---
+++ PairingAssignment.py
@@ -0,0 +1,5 @@
+class PairingAssignment:
+  
+   def __init__(self, id):
+       self.id = id
+      
---
+++ StandybyAssignment.py
@@ -0,0 +1,5 @@
+class StandbyAssignment:
+  
+   def __init__(self, id):
+       self.id = id
+      
